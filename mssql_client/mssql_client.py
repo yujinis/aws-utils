81f7a99513f72fe00aa1a8fb7c849b3b4d7db56f
Yuji Nishida <yujinis@amazon.co.jp>
2018-01-06 01:56:45 +0900

    It worked.

--- mssql_client/mssql_client.py
+++ mssql_client/mssql_client.py
@@ -1,24 +1,18 @@
 import pymssql
 import pyodbc
-import sys
+import sys,time
 
-ite_number = 100000
+iter_count = 100000
 
-c64 = """
-aaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggg
-"""
+c64 = "aaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggg"
 
 rc64 = c64[::-1]
 
-c256 = """
-aaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffggggaaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffggggaaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffggggaaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggg
-"""
+c256 = "aaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffggggaaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffggggaaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffggggaaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggg"
 
 rc256 = c256[::-1]
 
-text = """
-ああああああああああいいいいいいいいいいううううううううううええええええええええおおおおおおおおおおああああああああああいいいいいいいいいいううううううううううええええええええええおおおおおおおおおお
-"""
+text = "ああああああああああいいいいいいいいいいううううううううううええええええええええおおおおおおおおおおああああああああああいいいいいいいいいいううううううううううええええええええええおおおおおおおおおお"
 
 rtext = text[::-1]
 
@@ -29,27 +23,27 @@ class PyComm:
         self.inserts_sleep = t2
 
     def sleep_each_insert(self):
-        sleep(self.insert_sleep)
+        time.sleep(self.insert_sleep)
 
     def sleep_each_inserts(self):
-        sleep(self.inserts_sleep)
+        time.sleep(self.inserts_sleep)
 
-    def get_itarative_number(self):
+    def get_iter_count(self):
         if self.mode == "test":
             return 2
         else:
-            return ite_number
+            return iter_count
 
 class PyMSSQL(PyComm):
 
     def __init__(self,t1=0,t2=0):
         super().__init__(t1,t2)
 
-    def create_connection(self,h,p,u,w,d):
+    def create_connection(self,h,u,w,d):
         return db.create(host=h,port=p,user=u,password=w,database=d)
     
-    def create_databsase(self,cnn):
-        cnn.execute("CREATE DATABASE test_db IF NOT EXIST");
+    def create_databsase(self,conn):
+        conn.execute("CREATE DATABASE test_db IF NOT EXIST");
 
 
 class PyODBC(PyComm):
@@ -57,55 +51,72 @@ class PyODBC(PyComm):
     def __init__(self,t1=0,t2=0):
         super().__init__(t1,t2)
 
-    def create_connection(self,h,_,u,w,d):
-        return pyodbc.connect("DSN={0};UID={1};PWD={2};DATABASE={3}".format(h,u,w,d))
+    def create_connection(self,dsn,u,w,d):
+        print("create_connection")
+        return pyodbc.connect("DSN={0};UID={1};PWD={2};DATABASE={3}".format(dsn,u,w,d), timeout=10)
     
-    def create_databsase(self,cnn):
-        cnn.execute("CREATE DATABASE test_db IF NOT EXIST");
+    def create_databsase(self,conn):
+        conn.execute("CREATE DATABASE test_db IF NOT EXIST");
  
-    def create_table(self,cnn):
+    def drop_table(self,conn):
+        print("drop_table")
         cur = conn.cursor()
-        cur.execute("""
-        CREATE TABLE test_table IF NOT EXIST (
-            a_int INT(10),
-            b_varchar VARCHAR(64),
-            c_varchar VARCHAR(256),
-            d_text    TEXT,
-            PRIMARY   KEY (a_int)
-        )
-        """)
-        cnn.commit()
+        cur.execute("DROP TABLE test_table")
+        conn.commit()
 
-    def insert_data(self,cnn):
-        for i in range(100000):
-            self.insert_data_unit(self,cnn,i)
+    def create_table(self,conn):
+        print("create_table")
+        cur = conn.cursor()
+        try:
+            cur.execute("""
+            CREATE TABLE test_table (
+                a_int INT NOT NULL,
+                b_char CHAR(64),
+                c_char CHAR(256),
+                d_nvarchar NVARCHAR(256),
+                PRIMARY KEY (a_int)
+            )
+            """)
+        except pyodbc.ProgrammingError as e:
+            print(e)
+            if "There is already an object" in e.args[1]:
+                pass
+            else:
+                sys.exit(-1)
+        else:
+            conn.commit()
 
-    def insert_data_unit(self,cnn,i):
+    def insert_data(self,conn):
+        print("insert_data : {0}".format(self.get_iter_count()))
+        for i in range(self.get_iter_count()):
+            self.insert_data_unit(conn,i)
+
+    def insert_data_unit(self,conn,i):
         cur = conn.cursor()
         cur.execute("""
         INSERT INTO test_table (
             a_int,
-            b_varchar,
-            c_varchar,
-            d_text
-        ) VALUES (
+            b_char,
+            c_char,
+            d_nvarchar) VALUES (
             {0},'{1}','{2}','{3}'
         )
         """.format(i,c64,c256,text))
-        cnn.commit()
+        conn.commit()
 
-    def update_data(self,cnn):
-        for i in range(100000):
-            self,update_data_unit(cnn,i)
+    def update_data(self,conn):
+        print("update_data : {0}".format(self.get_iter_count()))
+        for i in range(self.get_iter_count()):
+            self.update_data_unit(conn,i)
             self.sleep_each_insert()
 
         self.sleep_each_inserts()
 
-        for i in range(100000):
-            self,update_data_unit(cnn,i,rev=True)
+        for i in range(self.get_iter_count()):
+            self.update_data_unit(conn,i,rev=True)
             self.sleep_each_insert()
 
-    def update_data_unit(self,cnn,i,rev=False):
+    def update_data_unit(self,conn,i,rev=False):
         if rev:
             _c64  = rc64
             _c256 = rc256
@@ -116,29 +127,34 @@ class PyODBC(PyComm):
             _text = text
 
         cur = conn.cursor()
-        cnn.execute("""
+        conn.execute("""
         UPDATE test_table SET
-        b_varchar='{0}',
-        c_varchar='(1)',
-        d_text='(2)'
+        b_char='{0}',
+        c_char='(1)',
+        d_nvarchar='(2)'
         WHERE a_int = {3}
         """.format(_c64,_c256,_text,i))
-        cnn.commit()
+        conn.commit()
 
-####################
 
 if __name__ == "__main__":
 
-    if 5 < len(sys.argv):
+    #print(len(c64))
+    #print(len(c256))
+    #print(len(rtext))
+
+    if 4 < len(sys.argv):
 
         #db = PyMSSQL()
         db = PyODBC()
         #db.mode = "real"
-        cnn = db.create_connection(*sys.argv[1:])
+        conn = db.create_connection(*sys.argv[1:])
         #db.create_database()
-        db.create_table(cnn)
-        db.insert_data(cnn)
-        db.update_data(cnn)
+        db.drop_table(conn)
+        db.create_table(conn)
+        db.insert_data(conn)
+        db.update_data(conn)
 
     else:
-        print("python "+sys.argv[0]+" xxx.abcdefg.ap-northeast-1.rds.amazonaws.com 1433 root pass database")
+        print("(For pyodbc) python "+sys.argv[0]+" DSN root pass database")
+
